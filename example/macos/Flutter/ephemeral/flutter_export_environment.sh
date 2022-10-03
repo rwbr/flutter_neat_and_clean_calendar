5b103834d2931bb7113b0d50d07953043cfba0b8
Ralf Weinbrecher <rweinbrecher@agfeo.de>
2022-10-03 18:02:58 +0200

    Update example for macOS build

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ralfweinbrecher/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ralfweinbrecher/Development/work/flutter-projects/flutter_neat_and_clean_calendar/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=2"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ example/macos/Pods/Target Support Files/macos_ui/macos_ui-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_macos_ui : NSObject
+@end
+@implementation PodsDummy_macos_ui
+@end
